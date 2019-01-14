4c6343d2990a1dd846bf3a26ae6d48723f64f1b0
Alex <alex.garcia.arias@gmail.com>
2019-01-14 14:36:55 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://127.0.0.1:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Saga#1992@ds153814.mlab.com:53814/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;

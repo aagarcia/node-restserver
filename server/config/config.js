96ba516323684bb312364dcfb5ff49c0c195705f
Alex <alex.garcia.arias@gmail.com>
2019-01-14 12:14:45 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
  * Puerto
  */
 process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * Entorno
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/**
+ * Base de datos
+ */
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://127.0.0.1:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Saga#1992@ds153814.mlab.com:53814/cafe';
+}
+
+process.env.URLDB = urlDB;
